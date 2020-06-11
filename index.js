6321cb06c7fe64c8b549e6abe875c9889f0ba0e6
johnlindquist <johnlindquist@gmail.com>
2020-06-11 14:41:04 -0600

    🎈

---
+++ index.js
@@ -0,0 +1,14 @@
+let alfred = {
+    "items": [
+        {
+            arg: "John Lindquist",
+            icon: {
+                path: "/Users/johnlindquist/Downloads/PNG/Asset 9.png"
+            },
+            title: "John",
+            subtitle: "Lindquist"
+        }
+    ]
+}
+
+console.log(JSON.stringify(alfred))
